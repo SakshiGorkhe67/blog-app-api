1ba7e2d22db5cca267ded49f583f2e89be004e45
Sakshi <sakshigorkhe67@gmail.com>
2026-04-18 02:50:56 +0530

    Searching

---
+++ src/main/java/com/codewithdurgesh/blog/services/FileService.java
@@ -0,0 +1,17 @@
+package com.codewithdurgesh.blog.services;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+public interface FileService {
+
+    //Upload Image
+    String uploadImage(String path, MultipartFile file)throws IOException;
+
+    //Serve Image
+    InputStream getResource(String path,String fileName)throws FileNotFoundException;
+}
+
